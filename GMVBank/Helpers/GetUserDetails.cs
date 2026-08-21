063e0456914cd9c2ab8c65db6d0f7deb7520a349
z005596v <giridhar.m-v@siemens.com>
2026-08-21 21:48:38 +0530

    Creating Solution: 1.Creating Helper folder and files. 2.Having a Externsion file. 3.Separate Interface file. 4.Getting the User details is done.

---
+++ GMVBank/Helpers/GetUserDetails.cs
@@ -0,0 +1,134 @@
+﻿using GMVBank.Interface;
+
+namespace GMVBank.Helpers
+{
+    public class GetUserDetails: IUserData
+    {
+        #region Singleton Implementation
+
+        private static GetUserDetails _instance;
+        public static GetUserDetails Instance => _instance;
+        public GetUserDetails() { }
+        #endregion
+
+        #region Private Fields
+
+        int _userAccType;
+        string _userGender = "male";
+        string _name = "Giri";
+        string _accountNumber = "912837483";
+        public int AccountType
+        {
+            get => _userAccType;
+            set => _userAccType = value;
+        }
+        public string Gender
+        {
+            get => _userGender;
+            set => _userGender = value;
+        }
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+        public string AccountNumber
+        {
+            get => _accountNumber;
+            set => _accountNumber = value;
+        }
+        #endregion
+
+        #region User Account Number
+        /// <summary>
+        /// Gets the account number of the user. It prompts the user to enter their account number and ensures that 
+        /// the input is not empty. If the input is invalid, it catches the exception and prompts the user to enter a valid input again.
+        /// </summary>
+        public void GetUserAccountNumber()
+        {
+            try
+            {
+                Console.WriteLine("Please Enter AccountNumber:\n");
+                AccountNumber = Console.ReadLine();
+                Extension.Instance.EnsureStringNotEmpty(AccountNumber);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex}, Please enter valid input.");
+                GetUserAccountNumber();
+            }
+        }
+        #endregion
+
+        #region UserName
+        /// <summary>
+        /// Gets the name of the user. It prompts the user to enter their name as per Aadhaar and ensures that the input 
+        /// is not empty. If the input is invalid, it catches the exception and prompts the user to enter a valid input again.
+        /// </summary>
+        public void GetUserName()
+        {
+            try
+            {
+                Console.WriteLine("Please enter Name as per Aadhaar:\n");
+                Name = Console.ReadLine();
+                Extension.Instance.EnsureStringNotEmpty(Name);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex}, Please enter valid input.");
+                GetUserName();
+            }
+        }
+        #endregion
+
+        #region UserGender
+        /// <summary>
+        /// Gets the sex of the user.
+        /// </summary>
+        public void GetUserGender()
+        {
+            try
+            {
+                Console.WriteLine("Enter Gender:\n");
+                Console.WriteLine(string.Join("\n", OptionHelper.GenderType.Values));
+                Gender = Console.ReadLine();
+                Extension.Instance.EnsureStringNotEmpty(Gender);
+                OptionHelper.GenderType.TryGetValue(Gender, out string gender);
+                Gender = gender;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex}, Please enter valid input.");
+                GetUserGender();
+            }
+        }
+        #endregion
+
+        #region GetUserAccountType
+        /// <summary>
+        /// This method is used to get the account type from the user. It takes an Int16 parameter accType and prompts the user to 
+        ///enter the account type. The user can choose between Current Account and Saving Account by entering 1 or 2 respectively. 
+        ///The method then assigns the entered value to the accType parameter.
+        /// </summary>
+        /// <param name="accType"></param>
+        public void GetUserAccountType()
+        {
+            try
+            {
+                Console.WriteLine("Please Enter AccountType: ");
+                Console.WriteLine(string.Join("\n", OptionHelper.AccTypes.Values));
+                AccountType = Convert.ToInt32(Console.ReadLine());
+                AccountType = Extension.Instance.EnsureIntegerInRange(AccountType, 0, OptionHelper.AccTypes.Count);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex}, Please enter valid input.");
+                GetUserAccountType();
+            }
+            Console.WriteLine($"You have selected {OptionHelper.AccTypes[AccountType]}");
+        }
+
+        #endregion
+    }
+}
---
+++ GMVBank/Program.cs
@@ -0,0 +1,36 @@
+﻿using GMVBank.Helpers;
+using GMVBank.Interface;
+using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
+using System.Security.Principal;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace GMVBank
+{
+    public class Program
+    {
+       
+
+        #region Main Method
+        static void Main()
+        {
+            Console.WriteLine("************* WELCOME TO GMVBANK *************");
+
+            Program p = new Program(); // Create an instance of the Program class
+
+            p.UserDetails(); // Call the GetUserDetails method to get user details
+        }
+        #endregion
+        /// <summary>
+        /// calls the GetUserDetails class to get the user details like name, account number, account
+        /// </summary>
+        public void UserDetails()
+        {
+            GetUserDetails.Instance.GetUserName();
+            GetUserDetails.Instance.GetUserAccountNumber();
+            GetUserDetails.Instance.GetUserAccountType();
+            GetUserDetails.Instance.GetUserGender();
+        }
+    }
+}
